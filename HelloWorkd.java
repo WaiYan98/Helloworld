01dcd5666ba487b0cba733fb1f4cf42ef4a8432e
muhammadanis418 <67187621+muhammadanis418@users.noreply.github.com>
2020-10-01 21:12:24 +0500

    Update HelloWorkd.java

--- HelloWorkd.java
+++ HelloWorkd.java
@@ -1,7 +1,9 @@
-class HelloWorld{
+// adding comments on code can make code easier
 
-	public static void main(String[]args){
+class HelloWorld{    // this line represent class name hellow world 
+
+	public static void main(String[]args){      // main has been started from here
 		
-		System.out.println("Hello World");
+		System.out.println("Hello World"); // by using that line we can print hellow world as output
 	}
 }
